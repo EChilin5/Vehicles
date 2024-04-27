7000526d21b5f577898288d0cb874f15996d4ae5
EChilin5 <edgar3ac@gmail.com>
2024-04-26 17:49:13 -0700

    parking lot additional content

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 
 public class ParkingLot {
-    int defaultParkingSpots = 30;
+    static int defaultParkingSpots = 30;
 
-    private ArrayList<Vehicle> vehiclesSpots = new ArrayList<>();
-    public int getDefaultParkingSpots(){
+    public static ArrayList<Vehicle> vehiclesSpots = new ArrayList<>();
+    public static int getDefaultParkingSpots(){
         return defaultParkingSpots;
     }
-    public int getAvailableSpaces(){
+    public static int getAvailableSpaces(){
         return defaultParkingSpots - vehiclesSpots.size();
     }
 
-    public boolean add(Vehicle vehicle){
-        if(getDefaultParkingSpots() < 30){
+    public static boolean add(Vehicle vehicle){
+        if(getAvailableSpaces() <= 30){
             vehiclesSpots.add(vehicle);
             return true;
         }
         return false;
     }
 
-    public boolean search(Vehicle vehicle){
+    public static boolean search(Vehicle vehicle){
         for(int i = 0; i < vehiclesSpots.size(); i++){
             String license = vehiclesSpots.get(i).getLicense();
             if(license == vehicle.getLicense()){
@@ -29,7 +29,7 @@ public class ParkingLot {
         return false;
     }
 
-    public boolean remove(Vehicle vehicle){
+    public static boolean remove(Vehicle vehicle){
         for(int i = 0; i < vehiclesSpots.size(); i++){
             String license = vehiclesSpots.get(i).getLicense();
             if(license == vehicle.getLicense()){
@@ -38,5 +38,19 @@ public class ParkingLot {
             }
         }
         return false;
+    }
+
+    public static void main(String[] args){
+        Car car1 = new Car("tekgken", Size.Medium);
+        Car car2 = new Car("rerewrr", Size.Medium);
+        Car car3 = new Car("rervfsr", Size.Medium);
+        System.out.println("Added Car1 -> " + add(car1));
+        System.out.println("Added Car2 -> " +add(car2));
+        System.out.println("Added Car3 -> " + add(car3));
+        System.out.println("Search Car2 " + search(car2));
+        System.out.println("Delete Car1 -> " + remove(car1));
+        System.out.println("Search Car1 " + search(car1));
+
+
     }
 }
